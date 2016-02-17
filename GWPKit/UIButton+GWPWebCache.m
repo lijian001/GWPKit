f7ad230d4c9e76e6cdfc94fb71b08719d8dca5cc
GanWenPeng <ganwenpeng@GanWenPengs-Mac-mini.local>
2016-02-17 10:07:09 +0800

    解决UIButton图片缓存image、backgroundImage反掉了的BUG

--- GWPKit/UIButton+GWPWebCache.m
+++ GWPKit/UIButton+GWPWebCache.m
@@ -11,18 +11,19 @@
 
 @implementation UIButton (GWPWebCache)
 - (void)gwp_setBackgroundImageWithURL:(NSURL *)url forState:(UIControlState)state{
-    [self sd_setImageWithURL:url forState:state];
-}
-
-- (void)gwp_setBackgroundImageWithURL:(NSURL *)url forState:(UIControlState)state placeholderImage:(UIImage *)placeholderImage{
-    [self sd_setImageWithURL:url forState:state placeholderImage:placeholderImage];
-}
-
-- (void)gwp_setImageWithURL:(NSURL *)url forState:(UIControlState)state{
     [self sd_setBackgroundImageWithURL:url forState:state];
 }
 
-- (void)gwp_setImageWithURL:(NSURL *)url forState:(UIControlState)state placeholderImage:(UIImage *)placeholderImage{
+- (void)gwp_setBackgroundImageWithURL:(NSURL *)url forState:(UIControlState)state placeholderImage:(UIImage *)placeholderImage{
+    
     [self sd_setBackgroundImageWithURL:url forState:state placeholderImage:placeholderImage];
+}
+
+- (void)gwp_setImageWithURL:(NSURL *)url forState:(UIControlState)state{
+    [self sd_setImageWithURL:url forState:state];
+}
+
+- (void)gwp_setImageWithURL:(NSURL *)url forState:(UIControlState)state placeholderImage:(UIImage *)placeholderImage{
+    [self sd_setImageWithURL:url forState:state placeholderImage:placeholderImage];
 }
 @end
